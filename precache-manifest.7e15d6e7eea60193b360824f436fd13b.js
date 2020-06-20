1ae965f442a9043089ee8705309acead6a63c17d
Sanchari Dasgupta <sanchari.dasgupta21@gmail.com>
2020-06-21 00:41:44 +0530

    Updates

---
+++ precache-manifest.7e15d6e7eea60193b360824f436fd13b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961d9f691837eaadafaf58b00cc464d3",
+    "url": "/React_portfolio/index.html"
+  },
+  {
+    "revision": "777606a284d776e1cc12",
+    "url": "/React_portfolio/static/css/main.96a3621e.chunk.css"
+  },
+  {
+    "revision": "beb3f3a03c03bb6aa808",
+    "url": "/React_portfolio/static/js/2.b44b6348.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_portfolio/static/js/2.b44b6348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "777606a284d776e1cc12",
+    "url": "/React_portfolio/static/js/main.291f666c.chunk.js"
+  },
+  {
+    "revision": "8ba33022e9690c980fcd",
+    "url": "/React_portfolio/static/js/runtime-main.0aee5354.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0af0dca55e768772e429b69f028a36ed",
+    "url": "/React_portfolio/static/media/slef.0af0dca5.jpg"
+  }
+]);
